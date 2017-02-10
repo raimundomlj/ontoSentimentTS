3343061b0692064fa20c38e5568b3d393862623d
raimundo.martins <raimundo.martins@10.3.222.127>
2017-02-10 17:34:10 -0300

    new files

---
+++ ontoSentiment/src/ontoSentiment/ImprimeArquivo.java
@@ -0,0 +1,47 @@
+package ontoSentiment;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class ImprimeArquivo extends Thread{
+
+	private String nome;
+	ArrayList<TweetTraduzido> dados;
+	
+	public ImprimeArquivo(String nome, ArrayList<TweetTraduzido> dados){
+		this.nome = nome;
+		this.dados = dados;
+	}
+	public void run() {
+		try {
+			FileWriter arquivo = new FileWriter(new File("C:/Users/raimundo.martins/Desktop/"+nome+".csv"));
+			//BufferedWriter arquivo = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:/Users/Raimundo/Desktop/"+nome+".txt"),"ISO-8859-1"));
+			String print = "";
+			for(TweetTraduzido d : dados){
+				print = d.getPt()+";";
+				
+				if(d.getEnGoogle() !=null && !d.getEnGoogle().equals(""))
+					print += d.getEnGoogle()+";";
+				else
+					print += "<none>;";
+				
+				if(d.getEnYandex() !=null && !d.getEnYandex().equals(""))
+					print += d.getEnYandex()+";";
+				else
+					print += "<none>;";
+				
+				print +=d.getClassifiedGoogle()+";";
+				print +=d.getClassifiedYandex();				
+				arquivo.write(print+";\n");
+				
+			}
+			arquivo.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ ontoSentiment/src/ontoSentiment/TweetTraduzido.java
@@ -0,0 +1,40 @@
+package ontoSentiment;
+
+public class TweetTraduzido {
+	private String pt;
+	private String enGoogle;
+	private String enYandex;
+	private String classifiedGoogle;
+	private String classifiedYandex;
+	
+	public String getPt() {
+		return pt;
+	}
+	public void setPt(String pt) {
+		this.pt = pt;
+	}
+	public String getEnGoogle() {
+		return enGoogle;
+	}
+	public void setEnGoogle(String enGoogle) {
+		this.enGoogle = enGoogle;
+	}
+	public String getEnYandex() {
+		return enYandex;
+	}
+	public void setEnYandex(String enYandex) {
+		this.enYandex = enYandex;
+	}
+	public String getClassifiedGoogle() {
+		return classifiedGoogle;
+	}
+	public void setClassifiedGoogle(String classifiedGoogle) {
+		this.classifiedGoogle = classifiedGoogle;
+	}
+	public String getClassifiedYandex() {
+		return classifiedYandex;
+	}
+	public void setClassifiedYandex(String classifiedYandex) {
+		this.classifiedYandex = classifiedYandex;
+	}
+}
