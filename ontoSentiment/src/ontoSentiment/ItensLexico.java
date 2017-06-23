cf63e5ea1b7726526074f3e515c9e315fcab1925
raimundo.martins <raimundo.martins@CPX-L5WU3YAD5HS.dir.svc.accenture.com>
2017-06-23 16:33:55 -0300

    nova classe para lexico

---
+++ ontoSentiment/src/ontoSentiment/ItensLexico.java
@@ -0,0 +1,35 @@
+package ontoSentiment;
+
+public class ItensLexico {
+
+	private String palavra;
+	private String classificacao;
+	private String pontos;
+	private String modo;
+	public String getPalavra() {
+		return palavra;
+	}
+	public void setPalavra(String palavra) {
+		this.palavra = palavra;
+	}
+	public String getClassificacao() {
+		return classificacao;
+	}
+	public void setClassificacao(String classificacao) {
+		this.classificacao = classificacao;
+	}
+	public String getPontos() {
+		return pontos;
+	}
+	public void setPontos(String pontos) {
+		this.pontos = pontos;
+	}
+	public String getModo() {
+		return modo;
+	}
+	public void setModo(String modo) {
+		this.modo = modo;
+	}
+	
+	
+}
